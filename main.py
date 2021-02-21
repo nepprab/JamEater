01731c204dab373c343c5e65d90ea6f0fa042f7d
ProGamer368 <62499031+ProGamer368@users.noreply.github.com>
2021-02-21 15:43:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import discord
 from discord.ext import commands
 import os
 import random
+import json
 
+with open("bot_config/config.json") as f:
+   config = json.load(f)
+
+# defining a few stuff...
 intents = discord.Intents.all()
 intents.members = True
 intents.bans = True
 intents.typing = True
 bot = commands.Bot(command_prefix="jm ", case_insensitive=True, intents=intents)
+token = config['bot-token']
+bot.owner_ids = config['bot-owners']
 
 @bot.event
 async def on_ready():
